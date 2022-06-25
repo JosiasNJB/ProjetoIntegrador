57bc323b3ce326094aa2499b4d6d17cdcd97a122
JosiasNJB <84824433+JosiasNJB@users.noreply.github.com>
2022-06-25 20:38:53 -0300

    Update header.php

--- ProjetoIntegrador-main/site_JLV/header.php
+++ ProjetoIntegrador-main/site_JLV/header.php
@@ -3,6 +3,7 @@
 <head>
  	<link rel="stylesheet" href="css/styles.css">
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 
@@ -22,13 +23,18 @@
 
 			$resultado = mysqli_query($connect, $sql);
 
-			$array = mysqli_fetch_array($resultado);	
-
+			$array = mysqli_fetch_array($resultado);
+			
 			$nome = $array[0];
+
+			$perfil = "<li><div class='colordropd'><div class='dropdown'> <button class='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>$nome </button> <ul class='dropdown-menu' aria-labelledby='dropdownMenuLink'><li><a class='dropdown-item' href='logout.php'>Log Out</a></li></ul></div><div></li>";
+
+			$onoff = "";
 
 		}
 		else{
-			$nome = "";
+			$perfil = "";
+			$onoff = "<li><a href='login.php'>Login</a></li>";
 		}
 		
 	?>
@@ -37,23 +43,24 @@
 </head>
 <body>
 
-	<!-- A tag <header> é o nosso cabeçalho, mas dessa vez aparecerá.-->
+	<!- A tag <header> é o nosso cabeçalho, mas dessa vez aparecerá.-->
 	<header>
 
 		<!-- A tag <figure> serve para marcar diagramas, ilustrações, fotos, e fragmentos de código.-->
 		<figure>
-			<a href="index.php"><img src="img/icons8-forest-30.png" alt="Yby"></a>
+			<a href="index.php"><img src="img/icon-forest-0.png" id="treeimg"alt="Yby"></a>
 			<td> Yby </td> 
 		</figure> 
 			
 		<div class="menu">
+
 			<li><a href="index.php">Home</a></li>
 			<li><a href="sobre.php">Sobre</a></li>
-			<li><a href="login.php">Login</a></li>
+			<?php echo $onoff; ?>
 			<li><a href="contato.php">Contato</a></li>
 			<li><a href="depoimentos.php">Depoimentos</a></li>
 			<li><a href="usuarios.php">Usuários</a></li>
-			<li> <?php echo $nome; ?></li>
+			<?php echo $perfil ?>
 			
 		</div>
 			
